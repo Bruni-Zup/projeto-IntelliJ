afa38d56c65bd6bdee22186df521daca06fb4612
Bruni <bruniely.marchioro@zup.com.br>
2024-12-04 15:21:45 -0300

    cria primeira variável em java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Meus Dados {
+    public static void main(String[] args) {
+        String nome = "Bruniely";
+        int idade = 36;
+        double altura = 1.72;
+        boolean matriculado = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Está matriculado no curso? " + matriculado);
+    }
+}
